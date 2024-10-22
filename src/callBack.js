e6823e7552d23b3ff081eb29295349874a741fc6
dearguru07 <pangalaguruyadav101@gmail.com>
2024-10-22 09:14:40 +0530

    g

--- src/callBack.js
+++ src/callBack.js
@@ -1,9 +1,17 @@
-import React from "react";
+// import React, { useState } from "react";
 
-const callBack=()=>{
-    return <div>
-        
-    </div>
-}
+// const CallBack=()=>{
+//     const [one,setOne]=useState(0)
+//     const [two,setTwo]=useState(0)
+//     const [three,setTree]=useState(0)
+//     function First=()=>{
+//         setOne(one+1)
+//     }
+
+//     return <div>
+//         <h3>{one}</h3>
+//         <button onClick={First}>increse</button>
+//     </div>
+// }
 
 
